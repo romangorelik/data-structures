360ae1e385f74168e048080f05b38f8d3dbb5c33
Roman Gorelik <romang31@gmail.com>
2018-06-13 14:13:17 -0400

    Completed functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var counter = 0;
+  var count2 = 0;
+  var count3 = 0; 
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +10,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count3++] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    if (counter > 0) {
+      counter--;
+      var temp = storage[count2];
+      delete storage[count2];
+      count2++;
+      return temp;
+    }
   };
 
+  if (counter > 0) {
+    var temp = storage[--counter];
+    delete storage[counter];
+    return temp;
+  }
+
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
 };
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
+  var counter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter++] = value;
   };
 
   someInstance.pop = function() {
+    if (counter > 0) {
+      var temp = storage[--counter];
+      delete storage[counter];
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
