857a2bdf46681fb849214e9abe6f62310637203e
Roman Gorelik <romang31@gmail.com>
2018-06-13 17:15:58 -0400

    ES6 style completed.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.counter = 0;
+    this.count2 = 0;
+    this.count3 = 0;
+    this.storage = {};
+  };
 
+  enqueue (value) {
+    this.storage[this.count3++] = value;
+    this.counter++;
+  };
+
+  dequeue () {
+    if (this.counter > 0) {
+      this.counter--;
+      var temp = this.storage[this.count2];
+      delete this.storage[this.count2];
+      this.count2++;
+      return temp;
+    }
+  };
+
+  size () {
+    return this.counter;
+  };
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,23 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+
+  push(value) {
+    this.storage[this.counter++] = value;
+  };
+
+  pop() {
+    if (this.counter > 0) {
+      var temp = this.storage[--this.counter];
+      delete this.storage[this.counter];
+      return temp;
+    }
+  };
+
+  size () {
+    return this.counter;
+  }
 }
