d0f6abd6e51a039d2555057055738f43ac4a6acd
Roman Gorelik <romang31@gmail.com>
2018-06-14 11:37:02 -0400

    Completed Set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  var itemIndex = this._storage.indexOf(item);
+  var tempVal = this._storage[itemIndex];
+  this._storage.splice(itemIndex, 1);
+  return tempVal; 
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
