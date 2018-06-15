510d19be361533b3bddd88b2a20eb2b882ba9f74
Roman Gorelik <romang31@gmail.com>
2018-06-15 11:34:43 -0400

    2 Different ways of writing Hash.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,30 @@
 
+// var HashTable = function() {
+//   this._limit = 8;
+//   this._storage = LimitedArray(this._limit);
+// };
+
+// HashTable.prototype.insert = function(k, v) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   if (this._storage.get(index) === undefined) {
+//     this._storage.set(index, []);
+//   }
+//   this._storage.get(index)[k] = v;
+  
+// };
+
+// HashTable.prototype.retrieve = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   console.log(this._storage.get(index)[k]);
+//   return this._storage.get(index)[k];
+// };
+
+// HashTable.prototype.remove = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   delete this._storage.get(index)[k];
+// };
+
+
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -9,19 +35,34 @@ HashTable.prototype.insert = function(k, v) {
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, []);
   }
-  this._storage.get(index)[k] = v;
-  
+
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = v;
+      return;
+    } 
+  }
+
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  console.log(this._storage.get(index)[k]);
-  return this._storage.get(index)[k];
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      return this._storage.get(index)[i][1];
+    } 
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage.get(index)[k];
+  
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      delete this._storage.get(index)[i][0];
+    } 
+  }
 };
 
 
