65e47cef986943859f92ef565b1132b5b21f3331
Roman Gorelik <romang31@gmail.com>
2018-06-15 11:13:27 -0400

    HashTable complete.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,22 +6,46 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  this._storage.get(index)[k] = v;
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  console.log(this._storage.get(index)[k]);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  delete this._storage.get(index)[k];
 };
+
+
+// var HashTable = function() {
+//   this._limit = 8;
+//   this._storage = LimitedArray(this._limit);
+// };
+
+// HashTable.prototype.insert = function(k, v) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   var newHash = new HashTable();
+
+//   if (this._storage[index] === undefined) {
+//     this._storage.set(index, v);
+//   } else if (this._storage.get(index) instanceof HashTable){
+//     this._storage.get(index).insert(k, v);
+//   } else {
+//     var temp = this._storage.get(index);
+//     this._storage.set(index, newHash);
+//   }
+// };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
---
+++ sprint-two/src/test.js
@@ -0,0 +1,9 @@
+var arr = [];
+arr[0] = 'A';
+arr[2] = 'B';
+
+console.log(arr.length);
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
